6eb0c61d91593e694cfcbfcba3e9a046ad7193fe
scooterblume <scooterblume@yahoo.com>
2017-07-14 20:29:43 -0400

    added pictures

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	//make all strings for jakes images
+	var len = 26;
+	var jakes = new Array(len);
+	for (var i =0; i <=len; i++){
+		if (i!=7 && i!= 10){
+			jakes[i]= "jake"+(i+1);
+		}
+		
+	}
+	console.log(jakes[0])
+  res.render('index', {
+   title: 'Happy Birthday!',
+   pic: jakes
+    });
 });
 
 module.exports = router;
